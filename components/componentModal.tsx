95cc8430f2fb53fd43a58c1bfda5ac8c08c7b150
Teubissi Samuel <samtebs07@gmail.com>
2025-09-23 11:15:20 +0100

    Product card 2/2: Cart Officiency

--- components/componentModal.tsx
+++ components/componentModal.tsx
@@ -9,7 +9,7 @@ import {
 } from '@heroui/modal'
 import { Button, ButtonProps } from '@heroui/button'
 import { Link } from '@heroui/link'
-import { ShoppingCart } from 'lucide-react'
+import { ShoppingCart, Trash2Icon } from 'lucide-react'
 import clsx from 'clsx'
 import { mz_button } from '@/components/primitives'
 import Produits from '../app/dataMazou.json'
@@ -48,7 +48,7 @@ export const Modal = () => {
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1 mz_Heading border-b-2 px-0 ml-6">
+              <ModalHeader className="flex flex-col gap-1 mz_Heading border-b-2 px-0 ml-4">
                 Votre Panier
               </ModalHeader>
               <ModalBody>
@@ -56,10 +56,12 @@ export const Modal = () => {
                   {[...Array(3)].map((_, i) => (
                     <div
                       key={i}
-                      className="flex gap-2 py-3 border-b-2 border-gray-300"
+                      className="flex gap-2 py-3 border-b-2 border-gray-300 "
                     >
-                      <div className="flex items-center px-2">{i + 1}</div>
-                      <div className="w-[158px] h-[158px] flex justify-center">
+                      <div className="hidden md:flex items-center px-2 font-bold">
+                        {i + 1}
+                      </div>
+                      <div className="min-h-[100px] w-auto flex justify-center basis-40 rounded-md overflow-hidden">
                         <img
                           src={product.img[i]}
                           alt={product.title}
@@ -74,25 +76,32 @@ export const Modal = () => {
                           className="w-full h-full"
                         /> */}
                       </div>
-                      <div className="flex flex-col justify-between">
+                      <div className="flex flex-col justify-between gap-1 flex-1">
                         <div className="flex flex-col gap-1">
-                          <div className="font-bold">{product.title}</div>
-                          <div className="text-small text-gray-700 space-x-2">
-                            {product.product_tags.map((tag, i) => (
+                          <div className="font-bold text-lg">
+                            {product.title}
+                          </div>
+                          <div className="text-small text-gray-700 flex gap-1 flex-wrap line-clamp-2">
+                            {product.product_tags.slice(0, 2).map((tag, i) => (
                               <span
                                 key={i}
-                                className="underline cursor-default"
+                                className="underline cursor-default min-w-fit"
                               >
                                 {tag}
                               </span>
                             ))}
                           </div>
                         </div>
-                        <div className="flex justify-between items-baseline">
-                          <span className="text-2xl font-bold text-brand-primary-500">
+                        <div className="flex justify-between items-center">
+                          <span className="text-2xl sm:text-3xl font-bold text-brand-primary-500 min-w-fit">
                             {formatPrice(product.new_price)} F
                           </span>
-                          <span>Retirer du panier</span>
+                          <div className="flex items-center gap-1 min-w-fit text-gray-700 hover:text-brand-primary-400">
+                            <Trash2Icon />
+                            <span className="hidden sm:inline-block">
+                              Retirer du panier
+                            </span>
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -100,9 +109,9 @@ export const Modal = () => {
                 </div>
               </ModalBody>
               <ModalFooter className="gap-10 border-t border-gray-300">
-                <div className="border-b-2 border-brand-primary-500 flex items-center px-4 py-1 gap-2">
+                <div className="border-b-2 border-brand-primary-500 flex items-center px-4 gap-2">
                   <span>TOTAL:</span>
-                  <span className="text-2xl font-bold">
+                  <span className="text-2xl font-bold  min-w-fit">
                     {formatPrice(Number(product.new_price) * 3)} F
                   </span>
                 </div>
